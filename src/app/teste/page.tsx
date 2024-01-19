619d132d55394d4a54ddc9c0e6264e3abd6b305f
Bruno Hammes da Cruz <bruno.cruz@defensoria.ro.def.br>
2024-01-19 12:13:32 -0400

    configuração inicial

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "@/app/page.module.css";
+
+export default function TestePage() {
+  return (
+    <main className={styles.main}>
+      <p>sou a pagina teste </p>
+    </main>
+  );
+}
